fixed overflow problem in getNormalizedLocationWeightsFast function

TensorFlow/CDHDConversionTransferLearing/cdhd.py
@@ -73,6 +73,9 @@ def _variable_with_weight_decay(name, shape, stddev, wd):
 
 def getNormalizedLocationWeightsFast(w):
   #Softmax
+  w_mean, w_var = tf.nn.moments(w, axes=[1,2])
+  w = tf.divide(tf.subtract(w, w_mean[:,None,None,:]), tf.sqrt(w_var[:,None,None,:]))
+
   a = tf.reduce_max(w, axis=(1,2))
   ew = tf.exp(tf.subtract(w, a[:,None,None,:]))
   sew = tf.reduce_sum(ew, axis=(1,2))
@@ -127,6 +130,7 @@ def columnActivation(aug_x, column_num, fwd_dict):
     biases = _variable_on_cpu('col' + str(column_num) + 'row1biases', [nfc], tf.constant_initializer(0.1), wd=0.0)
     a = tf.nn.bias_add(conv, biases)
     # a = tf.nn.relu(a, name=scope.name)
+    res['row1_act'] = a      #DELETE  
 
   if(column_num == 0):
     with tf.variable_scope('row2') as scope:
@@ -138,7 +142,8 @@ def columnActivation(aug_x, column_num, fwd_dict):
       conv = tf.nn.conv2d(a, kernel, [1, 1, 1, 1], padding='SAME')
       biases = _variable_on_cpu('row2biases', [nfc], tf.constant_initializer(0.1), wd=0.0)
       a = tf.nn.bias_add(conv, biases)
-      # a = tf.nn.relu(a, name=scope.name)  
+      # a = tf.nn.relu(a, name=scope.name)
+      res['row2_act'] = a      #DELETE    
 
     with tf.variable_scope('row3') as scope:
       kernel = _variable_with_weight_decay('row3weights',
@@ -148,7 +153,8 @@ def columnActivation(aug_x, column_num, fwd_dict):
       kernel = tf.multiply(kernel, 0.0250)      #line 327-334 in warpTrainCNNCDHDCentroidChainGridPredSharedRevFastExp3
       conv = tf.nn.conv2d(a, kernel, [1, 1, 1, 1], padding='SAME')
       biases = _variable_on_cpu('row3biases', [num_out_filters], tf.constant_initializer(0.1), wd=0.0)
-      a = tf.nn.bias_add(conv, biases)                
+      a = tf.nn.bias_add(conv, biases)
+      res['row3_act'] = a      #DELETE                    
   else:
     with tf.variable_scope('row2', reuse=True) as scope:
       kernel = _variable_with_weight_decay('row2weights',
@@ -160,6 +166,7 @@ def columnActivation(aug_x, column_num, fwd_dict):
       biases = _variable_on_cpu('row2biases', [nfc], tf.constant_initializer(0.1), wd=0.0)
       a = tf.nn.bias_add(conv, biases)
       # a = tf.nn.relu(a, name=scope.name)
+      res['row2_act'] = a      #DELETE    
 
     with tf.variable_scope('row3', reuse=True) as scope:
       kernel = _variable_with_weight_decay('row3weights',
@@ -169,14 +176,19 @@ def columnActivation(aug_x, column_num, fwd_dict):
       kernel = tf.multiply(kernel, 0.0250)      #line 327-334 in warpTrainCNNCDHDCentroidChainGridPredSharedRevFastExp3
       conv = tf.nn.conv2d(a, kernel, [1, 1, 1, 1], padding='SAME')
       biases = _variable_on_cpu('row3biases', [num_out_filters], tf.constant_initializer(0.1), wd=0.0)
-      a = tf.nn.bias_add(conv, biases)      
+      a = tf.nn.bias_add(conv, biases)  
+      res['row3_act'] = a      #DELETE        
 
   #e.g.: a shape: [10, 42, 32, 26], w shape: [10, 42, 32, 1]
   w = a[:, :, :, 0:1]
 
   #getNormalizedLocationWeightsFast() returns softmax of the activation w
+  # nw, ew, sew, sub, sub_min, s_max = getNormalizedLocationWeightsFast(w)
   nw = getNormalizedLocationWeightsFast(w)
 
+  res['w_act'] = w      #DELETE        
+  res['nw_act'] = nw      #DELETE        
+  
   # nw[0] = batch size, nw[1] = rows, nw[2] = columns 
   nw_reshape = tf.reshape(nw, [tf.shape(nw)[0], -1])
 
@@ -340,9 +352,15 @@ def doForwardPass(x, out_locs, gt_loc):
     res_aux['pc_shape_' + str(i)] = res_step['pc_shape']     #DELETE
     res_aux['out_locs_rs_shape_' + str(i)] = res_step['out_locs_rs_shape']     #DELETE
     res_aux['nw_reshape_shape_' + str(i)] = res_step['nw_reshape_shape']     #DELETE    
+    res_aux['row1_act_' + str(i)] = res_step['row1_act']     #DELETE    
+    res_aux['row2_act_' + str(i)] = res_step['row2_act']     #DELETE    
+    res_aux['row3_act_' + str(i)] = res_step['row3_act']     #DELETE   
+    res_aux['w_act_' + str(i)] = res_step['w_act']     #DELETE   
+    res_aux['nw_act_' + str(i)] = res_step['nw_act']     #DELETE   
     res_aux['x1_shape_' + str(i)] = tf.shape(x)[1]          #DELETE
     res_aux['x2_shape_' + str(i)] = tf.shape(x)[2]          #DELETE
     res_aux['x_shape_' + str(i)] = tf.shape(x)              #DELETE    
+    res_aux['pool4_val_' + str(i)] = x
 
     #Output of step 1 goes as input to step 2 and output of step 2 goes as input to step 3
     #TODO: check size of x_sans_xa after data is fed. Note: checked on 12/3/17 - passed
@@ -477,8 +495,6 @@ def getSharedParametersList():
   var_list = var_list + tf.get_collection('row2biases')  
   var_list = var_list + tf.get_collection('row3weights')
   var_list = var_list + tf.get_collection('row3biases')    
-  var_list = var_list + tf.get_collection('weights') 
-  var_list = var_list + tf.get_collection('biases')
 
   return var_list
 
@@ -490,10 +506,25 @@ def train(res_aux, global_step):
 
   ret_dict['pc_shape'] = res_aux['pc_shape_1']    #DELETE 
   ret_dict['out_locs_rs_shape'] = res_aux['out_locs_rs_shape_1']    #DELETE 
-  ret_dict['nw_reshape_shape'] = res_aux['nw_reshape_shape_1']    #DELETE   
+  ret_dict['nw_reshape_shape'] = res_aux['nw_reshape_shape_0']    #DELETE 
+  ret_dict['row1_act'] = res_aux['row1_act_0']    #DELETE     
+  ret_dict['row2_act'] = res_aux['row2_act_0']    #DELETE     
+  ret_dict['row3_act'] = res_aux['row3_act_0']    #DELETE     
+  ret_dict['w_act'] = res_aux['w_act_0']    #DELETE     
+  ret_dict['nw_act'] = res_aux['nw_act_0']    #DELETE     
   ret_dict['x1_shape_1'] = res_aux['x1_shape_1']    #DELETE
   ret_dict['x2_shape_1'] = res_aux['x2_shape_1']    #DELETE
-  ret_dict['x_shape_1'] = res_aux['x_shape_1']    #DELETE  
+  ret_dict['x_shape_1'] = res_aux['x_shape_1']    #DELETE 
+  ret_dict['pool4_val'] = res_aux['pool4_val_0']    #DELETE
+
+  var_list = []
+  var_list = var_list + tf.get_collection('col0row1weights')
+  var_list = var_list + tf.get_collection('col0row1biases')
+  var_list = var_list + tf.get_collection('col1row1weights')
+  var_list = var_list + tf.get_collection('col1row1biases')
+  var_list = var_list + tf.get_collection('col2row1weights')
+  var_list = var_list + tf.get_collection('col2row1biases')
+  var_list = var_list + getSharedParametersList()
 
   total_loss = tf.reduce_sum(res_aux['loss'])
   a_optimizer = tf.train.AdamOptimizer()
@@ -505,7 +536,7 @@ def train(res_aux, global_step):
     use_locking=False,
     name='Adam_Opt')
 
-  minimizer = a_optimizer.minimize(total_loss, global_step=global_step)
+  minimizer = a_optimizer.minimize(total_loss, global_step=global_step, var_list=var_list)
   ret_dict['minimizer'] = minimizer
 
   return ret_dict

TensorFlow/CDHDConversionTransferLearing/cdhd_input.py
@@ -11,8 +11,8 @@ import sys
 import skimage.transform
 import skimage.io
 
-data_dir = '../../../../../../CS503-Thesis/car_dataset/'
-#data_dir = '../../../../car_dataset/'
+# data_dir = '../../../../../car_dataset/'
+data_dir = '../../../../car_dataset/'
 max_im_side = 500
 init_padding = 32
 min_side = 64
@@ -56,8 +56,8 @@ def distorted_inputs(stats_dict, batch_size, anno_file_batch_rows):
   target_locs = warped_target_locs
   bbox_heights = warped_bbox_heights
 
-  x = np.arange(start_offset, padded_images.shape[1]+1, output_stride)
-  y = np.arange(start_offset, padded_images.shape[2]+1, output_stride)
+  x = np.arange(start_offset, padded_images.shape[1], output_stride)
+  y = np.arange(start_offset, padded_images.shape[2], output_stride)
 
   out_locs_list = []
   for xi in xrange(x.shape[0]):
@@ -126,8 +126,8 @@ def getImage(meta_rec, stats_dict):
     #TODO: check if this normalizaton is working. 
     # Checked on 2/10/18 - appears to be working. Normalized image pixel values are approximately [-1, +1]  
     # Mean subtraction not required here since VGG does mean subtraction
-    im = np.subtract(im, stats_dict['mean_pixel'])
-    im = np.divide(im, stats_dict['std_pixel'])
+    # im = np.subtract(im, stats_dict['mean_pixel'])
+    # im = np.divide(im, stats_dict['std_pixel'])
 
     im_size = np.asarray(im.shape)
     
@@ -243,14 +243,20 @@ def concatWithPadding(images, im_sizes, target_locs, bbox_heights):
     warp_ratio_1 = round(float(224)/(padded_image.shape[0]),2)
     warp_ratio_2 = round(float(224)/(padded_image.shape[1]),2)
 
+    # print('padded_image.shape[0]: ', padded_image.shape[0])
+    # print('padded_image.shape[1]: ', padded_image.shape[1])
+    # print('bbox_heights: ', bbox_heights[idx])
+
     warped_target_locs.append([target_locs[idx][0] * warp_ratio_1, target_locs[idx][1] * warp_ratio_2])
 
     # TODO: check if the below is correct or if warp_ratio_2 is he right multiplier
     warped_bbox_heights.append(bbox_heights[0] * warp_ratio_1)
 
     # Use to write image to disk with when skinage is used, works when mean is disabled
-    #skimage.io.imsave('car_ims_before/img_' + str(idx) + '.jpeg', padded_image)
-    #skimage.io.imsave('car_ims_after/img_' + str(idx) + '.jpeg', resized_img)
+    skimage.io.imsave('car_ims_before/img_' + str(idx) + '.jpeg', padded_image)
+    skimage.io.imsave('car_ims_after/img_' + str(idx) + '.jpeg', resized_img)
+    
+    # sys.exit()
 
     # Use to write image to disk with when PIL image is used
     # Image.fromarray(np.pad(images[idx], padding_tuple,'edge')).save('car_ims/img_' + str(idx) + '.jpg') #DELETE

TensorFlow/CDHDConversionTransferLearing/cdhd_train.py
@@ -10,6 +10,7 @@ from tensorflow.python import debug as tf_debug
 import sys
 from scipy.misc import imresize
 import vgg16
+# import utils
 
 data_dir = '../../../../../../CS503-Thesis/car_dataset/'
 #data_dir = '../../../../car_dataset/'
@@ -69,7 +70,6 @@ def computeNormalizationParameters():
   stats_dict = {'mean_pixel': mean_pixel, 'std_pixel': std_pixel}
 
   #store values so that there's no need to compute next time    
-  
 
   return stats_dict
 
@@ -111,37 +111,57 @@ def calculteNormalizedValidationDistance(pred, original, bbox_heights):
   # return average normalized distance for the batch of 10 images
   return total_normalized_distance/float(original.shape[0])
 
-global_step = tf.Variable(0, trainable=False, dtype=tf.int32)
-images = tf.placeholder(dtype=tf.float32, shape=[batch_size, 224, 224, 3])
-out_locs = tf.placeholder(dtype=tf.float32, shape=[None, 2])
-org_gt_coords = tf.placeholder(dtype=tf.float32, shape=[batch_size, 2])   
+# global_step = tf.Variable(0, trainable=False, dtype=tf.int32)
+# #images = tf.placeholder(dtype=tf.float32, shape=[batch_size, 224, 224, 3])
+# images = tf.placeholder(dtype=tf.float32, shape=[2, 224, 224, 3])
+# out_locs = tf.placeholder(dtype=tf.float32, shape=[None, 2])
+# org_gt_coords = tf.placeholder(dtype=tf.float32, shape=[batch_size, 2])   
 
-vgg = vgg16.Vgg16()
-vgg.build(images)
+# vgg = vgg16.Vgg16()
+# vgg.build(images)
 
-stats_dict = computeNormalizationParameters() 
+# stats_dict = computeNormalizationParameters() 
 
-vgg_pool4 = vgg.pool4
-print('*** pool 4 shape: ', vgg_pool4.get_shape().as_list())
+# res_aux = cdhd.doForwardPass(vgg.pool4, out_locs, org_gt_coords)
 
-#res_aux = cdhd.inference(vgg_pool2, out_locs, org_gt_coords)
-res_aux = cdhd.doForwardPass(vgg_pool4, out_locs, org_gt_coords)
+# ret_dict = cdhd.train(res_aux, global_step)
 
-ret_dict = cdhd.train(res_aux, global_step)
+# val_dict = cdhd.test(res_aux, global_step)
 
-val_dict = cdhd.test(res_aux, global_step)
+# init = tf.global_variables_initializer()
 
-init = tf.global_variables_initializer()
+# saver = tf.train.Saver(max_to_keep=20)
 
-saver = tf.train.Saver(max_to_keep=20)
 
 with tf.Session() as sess:
 # with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:  
+
+  global_step = tf.Variable(0, trainable=False, dtype=tf.int32)
+  images = tf.placeholder(dtype=tf.float32, shape=[batch_size, 224, 224, 3])
+  # images = tf.placeholder(dtype=tf.float32, shape=[2, 224, 224, 3])
+  out_locs = tf.placeholder(dtype=tf.float32, shape=[None, 2])
+  org_gt_coords = tf.placeholder(dtype=tf.float32, shape=[batch_size, 2])   
+
+  vgg = vgg16.Vgg16()
+  vgg.build(images)
+
+  stats_dict = computeNormalizationParameters() 
+
+  res_aux = cdhd.doForwardPass(vgg.pool4, out_locs, org_gt_coords)
+
+  ret_dict = cdhd.train(res_aux, global_step)
+
+  val_dict = cdhd.test(res_aux, global_step)
+
+  init = tf.global_variables_initializer()
+
+  saver = tf.train.Saver(max_to_keep=20)
+
   writer = tf.summary.FileWriter('./graphs', sess.graph)
   sess.run(init)
 
   # Restore variables from disk.
-  #saver.restore(sess, "./ckpt/model695.ckpt")
+  #saver.restore(sess, "./ckpt/model409.ckpt")
   #print("Model restored.")
 
   # Following two lines are for debugging
@@ -150,7 +170,6 @@ with tf.Session() as sess:
   # sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
 
   all_train_visible_idx = [x for x in xrange(0,total_visible_training_images)]
-  #random.shuffle(all_train_visible_idx)  
 
   for epoch in xrange(max_steps):
     start_time = time.time()
@@ -170,9 +189,24 @@ with tf.Session() as sess:
       #print('**** x1_shape_1: ', out_dict['x1_shape_1'])       #DELETE
       #print('**** x2_shape_1: ', out_dict['x2_shape_1'])       #DELETE
       #print('**** x_shape_1: ', out_dict['x_shape_1'])       #DELETE      
-      #print('**** pc shape: ', out_dict['pc_shape'].shape)       #DELETE
+      # print('**** pc shape: ', out_dict['pc_shape'].shape)       #DELETE
       #print('**** out_locs_rs_shape: ', out_dict['out_locs_rs_shape'].shape)       #DELETE
-      #print('**** nw_reshape_shape: ', out_dict['nw_reshape_shape'].shape)       #DELETE      
+      # print('**** nw_reshape_shape: ', out_dict['nw_reshape_shape'].shape)       #DELETE  
+      # print('**** row1_act: ', out_dict['row1_act'].shape)       #DELETE  
+      # print('**** row2_act: ', out_dict['row2_act'].shape)       #DELETE  
+      # print('**** row3_act shape: ', out_dict['row3_act'].shape)       #DELETE  
+      # print('**** row3_act: ', out_dict['row3_act'][2,8:12,4:9,0:1])       #DELETE  
+      # print('**** w_act_shape: ', out_dict['w_act'].shape)       #DELETE  
+      # print('**** nw_act_shape: ', out_dict['nw_act'].shape)       #DELETE  
+      # print('**** w_act: ', out_dict['w_act'][2,8:12,4:9,0])       #DELETE  
+      # print('**** ew_act: ', out_dict['ew_act'])       #DELETE  
+      # print('**** tf mean: ', out_dict['sew_act'])       #DELETE  
+      # print('**** nw_act: ', out_dict['nw_act'])       #DELETE  
+      # print('**** before: ', out_dict['sub_act'][2,8:9,4,0])       #DELETE  
+      # print('**** mean: ', out_dict['sub_min_act'])       #DELETE  
+      # print('**** after: ', out_dict['s_max_act'][2,8:12,4:9,0])       #DELETE  
+
+      # print('pool4 val: ', out_dict['pool4_val'])
 
       out_f = open('out_file.txt', 'a+')
       out_f.write(str(epoch) + ' ' + str(batch) + ' ' + str(out_dict['loss']) + '\n')
@@ -189,11 +223,6 @@ with tf.Session() as sess:
     out_f_epoch = open('out_epoch.txt', 'a+')
     out_f_epoch.write(str(epoch) + ' ' + str(out_dict['loss']) + '\n')
     out_f_epoch.close()    
-
-    out_im_epoch = open('out_image_name.txt', 'a+')
-    for img_name in anno_file_batch_rows:
-      out_im_epoch.write(str(epoch) + ' ' + str(img_name.split('|')[2]) + '\n')
-    out_im_epoch.close()        
 
     # Validation step
     if((epoch % 2) == 0):