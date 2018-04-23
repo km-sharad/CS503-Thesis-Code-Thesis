072bed16f29f3aaf13f2e397e58e88e77c2c4e2a
skumar <kumar_sharad@outlook.com>
2018-04-22 22:42:08 -0700

    added dropout

---
+++ TensorFlow/DWLeash-HandHoldingLeashTransferLearningDropout/leash_eval.py
@@ -0,0 +1,170 @@
+import leash
+import random
+import tensorflow as tf
+from PIL import Image
+from math import sqrt
+import numpy as np
+import time
+import leash_input
+from tensorflow.python import debug as tf_debug
+import sys
+from scipy.misc import imresize
+import vgg16
+
+data_dir = '../DWLeash-HandHoldingLeash-001/dog_walking_dataset/'
+# data_dir = 'dog_walking_dataset/'
+total_visible_training_images = 250    # Number of training images where car door handle is visible
+total_visible_test_images = 70 # Number of validation images where car door handle is visible
+stats_sample_size = 100                 # Number of images to calculate mean and sd
+batch_size = 10                         # Number of images to process in a batch
+max_im_side = 500
+
+def computeNormalizationParameters():
+
+  all_train_visible_idx = [x for x in xrange(0,total_visible_training_images)]
+  random.shuffle(all_train_visible_idx)
+  stats_sample_indexes = all_train_visible_idx[0:stats_sample_size]
+
+  anno_file_batch_rows = []
+  anno_file = open('leash_anno_training_data.txt')
+  anno_file_lines = anno_file.readlines()
+
+  for x in stats_sample_indexes:
+    anno_file_batch_rows.append(anno_file_lines[x])
+ 
+  mean_pixel = np.zeros(3);
+  mean_pixel_sq = np.zeros(3);
+  pixel_covar = np.zeros((3, 3));
+
+  num_pixel = 0
+  for image_idx in xrange(stats_sample_size):
+    image_filename = anno_file_batch_rows[image_idx].split('|')[4]
+    image = Image.open(data_dir + image_filename)
+
+    im = np.array(image, dtype=np.uint64)
+    if(len(im.shape) == 2):
+      #monochrome image, add the third channel
+      im = np.stack((image,)*3)
+      print('monochrome image for mean calc: ', image_filename)      
+
+    #scale
+    scale = round(max_im_side/float(np.amax(im.shape)),4)
+    im = imresize(im, scale, interp='bilinear')
+    im = im.astype(np.uint64)
+
+    #reshape
+    im = im.reshape(im.shape[0] * im.shape[1],im.shape[2])
+    npix = im.shape[0]
+
+    mean_pixel = mean_pixel * (float(num_pixel)/(float(num_pixel + npix))) \
+                    + np.sum(im, axis=0)/((float(num_pixel + npix)))
+    mean_pixel_sq = mean_pixel_sq * (float(num_pixel)/(float(num_pixel + npix))) \
+                    + np.sum(np.square(im), axis=0)/(float(num_pixel + npix))
+
+    num_pixel = num_pixel + npix;
+
+  epsilon = 0.001;
+  std_pixel = np.sqrt(mean_pixel_sq - np.square(mean_pixel)) + epsilon
+  stats_dict = {'mean_pixel': mean_pixel, 'std_pixel': std_pixel}
+
+  #store values so that there's no need to compute next time    
+  
+
+  return stats_dict
+
+def getTestImageMetaRecords():
+  all_test_visible_idx = [x for x in xrange(0, total_visible_test_images)]
+  random.shuffle(all_test_visible_idx)
+
+  test_anno_file_batch_rows = []
+  # test_anno_file = open('leash_anno_testing_data.txt')
+  test_anno_file = open('leash_anno_training_data.txt')
+  test_anno_file_lines = test_anno_file.readlines()  
+
+  for x in all_test_visible_idx:
+    test_anno_file_batch_rows.append(test_anno_file_lines[x])  
+
+  return test_anno_file_batch_rows  
+
+def calculteNormalizedTestDistance(pred, original, bbox_heights):
+  total_normalized_distance = 0
+  for i in xrange(original.shape[0]): 
+    # print(pred[i][0][0][0],pred[i][0][1][0],original[i][0], original[i][1], bbox_heights[i])    
+    mse = sqrt(pow((pred[i][0][0][0] - original[i][0]), 2) + pow((pred[i][0][1][0] - original[i][1]), 2))
+    normalized_dist = mse/float(bbox_heights[i])    
+    # print(normalized_dist)
+    total_normalized_distance = total_normalized_distance + normalized_dist
+
+  # return average normalized distance for the batch of 10 images
+  return total_normalized_distance/float(original.shape[0])
+
+global_step = tf.Variable(0, trainable=False, dtype=tf.int32)
+images = tf.placeholder(dtype=tf.float32, shape=[batch_size, 224, 224, 3])
+out_locs = tf.placeholder(dtype=tf.float32, shape=[None, 2])
+org_gt_coords = tf.placeholder(dtype=tf.float32, shape=[batch_size, 2])   
+
+stats_dict = computeNormalizationParameters() 
+
+vgg = vgg16.Vgg16()
+vgg.build(images)
+
+res_aux = leash.doForwardPass(vgg.pool4,out_locs,org_gt_coords)
+
+val_dict = leash.test(res_aux, global_step)
+
+init = tf.global_variables_initializer()
+
+saver = tf.train.Saver(max_to_keep=10)
+
+with tf.Session() as sess:
+  writer = tf.summary.FileWriter('./graphs', sess.graph)
+  sess.run(init)
+
+  # Restore variables from disk.
+  saver.restore(sess, "./ckpt/model4781.ckpt")
+  print("Model restored.")
+
+  # Following two lines are for debugging
+  # Use <code> python cdhd_train.py --debug </code> command to debug
+  # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+  # sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
+
+  all_train_visible_idx = [x for x in xrange(0,total_visible_training_images)]
+  random.shuffle(all_train_visible_idx)  
+
+  for norm_dist in [0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]:
+    print('norm_dist: ', norm_dist)
+    total_batch = 0
+    batch_le_norm_dist = 0
+
+    anno_file_batch_rows = getTestImageMetaRecords()
+
+    for batch in xrange(len(anno_file_batch_rows)/batch_size):
+      test_images, test_meta = leash_input.distorted_inputs(stats_dict, batch_size, \
+              anno_file_batch_rows[batch * batch_size : (batch * batch_size) + batch_size])      
+
+      test_dict = sess.run(val_dict, feed_dict =
+                                    {images: test_images, 
+                                    out_locs: test_meta['out_locs'],
+                                    org_gt_coords: test_meta['org_gt_coords']})
+      
+      out_f = open('tmp_test_file.txt', 'a+')
+      out_f.write('*** org: ' + ' ' + str(test_meta['org_gt_coords']))
+      out_f.write('*** pred: ' + ' ' + str(test_dict['pred_coord']))
+      out_f.close()
+      print('*** loss: ', test_dict['loss'])
+
+      avg_normalized_dist = calculteNormalizedTestDistance(test_dict['pred_coord'], 
+                                            test_meta['org_gt_coords'],
+                                            test_meta['bbox_heights'])
+
+      total_batch = total_batch + 1
+      if(round(avg_normalized_dist, 2) <= norm_dist):
+        batch_le_norm_dist = batch_le_norm_dist + 1
+
+    print(norm_dist, float(batch_le_norm_dist)/float(total_batch))
+    out_f = open('out_test_file.txt', 'a+')
+    out_f.write(str(norm_dist) + ' ' + str(float(batch_le_norm_dist)/float(total_batch)) + '\n')
+    out_f.close()    
+    
+  writer.close() 
---
+++ TensorFlow/DWLeash-HandHoldingLeashTransferLearningDropout/leash_input.py
@@ -0,0 +1,265 @@
+import tensorflow as tf
+import random
+import numpy as np
+from PIL import Image
+from numpy import array
+from math import log
+from math import exp
+from scipy.misc import imresize
+from scipy.ndimage import zoom
+import sys
+import skimage.transform
+import skimage.io
+
+data_dir = '../DWLeash-HandHoldingLeash-001/dog_walking_dataset/'
+#data_dir = '../../../../car_dataset/'
+max_im_side = 500
+init_padding = 32
+min_side = 64
+start_offset = 16
+output_stride = 16
+scaling_factor = round(500/float(3), 4)
+
+def distorted_inputs(stats_dict, batch_size, anno_file_batch_rows):
+  """Construct distorted input for CIFAR training using the Reader ops.
+
+  Args:
+    batch_size: Number of images per batch.
+
+  Returns:
+    images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
+    labels: Labels. 1D tensor of [batch_size] size.
+  """
+
+  images = []
+  target_locs = []
+  infos = []
+
+  for image_idx in xrange(batch_size):
+    #read and convert images into numpy array
+    meta_rec = anno_file_batch_rows[image_idx].split('|')
+    [image, target_loc, info] = getImage(meta_rec, stats_dict)
+
+    images.append(image)
+    target_locs.append(target_loc)
+    infos.append(info)
+
+  im_sizes = np.asarray([info['im_size'] for info in infos])
+  padding = np.asarray([info['padding'] for info in infos])
+  final_scale = np.asarray([info['final_scale'] for info in infos])
+  im_org = np.asarray([info['im_org'] for info in infos])
+  bbox_heights = np.asarray([info['bbox_height'] for info in infos])
+
+  padded_images, cat_padding, warped_target_locs, warped_bbox_heights = \
+          concatWithPadding(np.asarray(images), np.asarray(im_sizes), target_locs, bbox_heights)
+  padding = padding + cat_padding
+  target_locs = warped_target_locs
+  bbox_heights = warped_bbox_heights
+
+  x = np.arange(start_offset, padded_images.shape[1]+1, output_stride)
+  y = np.arange(start_offset, padded_images.shape[2]+1, output_stride)
+
+  out_locs_list = []
+  for xi in xrange(x.shape[0]):
+    for yi in xrange(y.shape[0]):
+      out_locs_list.append((x[xi], y[yi]))
+
+  out_locs = np.asarray(out_locs_list)
+
+  gt_coords = np.round(np.divide(np.subtract(np.asarray(target_locs),start_offset), 
+                  float((output_stride + 1))),2)
+  org_gt_coords = np.asarray(target_locs)
+
+  '''
+  out_locs = out_locs ./ opts.scaling_factor;
+  org_gt_coords = org_gt_coords ./ opts.scaling_factor;
+  aug_target_loc = aug_target_loc ./ opts.scaling_factor;
+  '''
+
+  #Scale only for training
+  # out_locs = np.round(np.divide(out_locs, scaling_factor),4)
+  # org_gt_coords = np.round(np.divide(org_gt_coords, scaling_factor),4)
+
+  meta_dict = {}
+  meta_dict['margins'] = []
+  meta_dict['out_locs'] = out_locs
+  meta_dict['out_locs_width'] = x.shape[0]
+  meta_dict['out_locs_height'] = y.shape[0]
+  meta_dict['scale'] = final_scale
+  meta_dict['gt_coords'] = gt_coords
+  meta_dict['org_gt_coords'] = org_gt_coords
+  meta_dict['im_org'] = im_org;
+  meta_dict['bbox_heights'] = bbox_heights;
+  #meta_dict['im_org_scaled'] = ?;
+  #meta_dict['torso_height'] = ?;
+
+  return padded_images, meta_dict
+
+def getImage(meta_rec, stats_dict):
+    im_meta_dict = {}
+    im_meta_dict['gt_x_coord'] = int(float(meta_rec[0]))    #holding leash x
+    im_meta_dict['gt_y_coord'] = int(float(meta_rec[1]))    #holding leash y
+    im_meta_dict['img_size'] = [int(i) for i in meta_rec[5].split(',')]
+    im_meta_dict['bbox'] = [int(i) for i in meta_rec[6].strip()[0:len(meta_rec[6]) - 1].split(',')]    
+
+    image = Image.open(data_dir + meta_rec[4])
+    # image = skimage.io.imread(data_dir + meta_rec[4])
+
+    im = np.array(image)
+    if(len(im.shape) == 2):
+      #monochrome image, add the third channel
+      im = np.stack((image,)*3)
+      print('monochrome image: ', meta_rec[4])
+
+    im_org = im     
+
+    #Compute the default scaling
+    scale = round(max_im_side/float(np.amax(im.shape)),4)    
+
+    [im, target_loc, aug_scale] = getAugmentedImage(im, im_meta_dict)
+
+    im_org_scaled = imresize(im, scale,interp='bilinear')
+    im = im_org_scaled
+    target_loc[0] = int(round(target_loc[0] * scale,0))    
+    target_loc[1] = int(round(target_loc[1] * scale,0))   
+
+    #TODO: check if this normalizaton is working. 
+    # Checked on 2/10/18 - appears to be working. Normalized image pixel values are approximately [-1, +1]  
+    # Mean subtraction not required here since VGG does mean subtraction
+    im = np.subtract(im, stats_dict['mean_pixel'])
+    im = np.divide(im, stats_dict['std_pixel'])
+
+    im_size = np.asarray(im.shape)
+    
+    padding = np.zeros(4) #T/B/L/R - top, bottom, left, right
+
+    padding = padding + init_padding;
+    target_loc = target_loc + init_padding
+    im_size[0] = im_size[0] + (2 * init_padding)    #Y-axis
+    im_size[1] = im_size[1] + (2 * init_padding)    #X-axis
+
+    #Pad if smaller than minimum size
+    min_side_padding = np.maximum(np.subtract(min_side - im_size[0:2],np.zeros(2)),np.zeros(2))
+
+    padding[1] = padding[1] + min_side_padding[0]
+    padding[3] = padding[3] + min_side_padding[1]
+    im_size[0] = im_size[0] + min_side_padding[0]
+    im_size[1] = im_size[1] + min_side_padding[1]
+
+    #Pad the image if necessary to get the final size correct
+    size_padding = computePaddingForImage(im_size, output_stride)
+    padding[1] = padding[1] + size_padding[0]
+    padding[3] = padding[3] + size_padding[1]
+    im_size[0] = im_size[0] + size_padding[0]
+    im_size[1] = im_size[1] + size_padding[1]    
+
+    padding = padding.astype(int)
+
+    # NOTE: For some images, the padding fails. Debug why
+    padding_tuple = ((padding[0],padding[1]), (padding[2],padding[3]), (0,0))
+    try:
+      im = np.pad(im, padding_tuple,'edge')
+      assert(np.array_equal(im_size, np.asarray(im.shape))), "assertion error in image preprocessing"
+    except ValueError:
+      # Assertion will fail in this case
+      print('padding error for image: ', meta_rec[4])
+
+    # Question: Only image is padded, target_loc is not padded, why?
+    # Answer: Because zero padding is done in bottom and right side of the image
+
+    final_scale = aug_scale * scale
+
+    info = {}
+    info['aug_scale'] = aug_scale
+    info['im_org'] = im_org
+    info['im_size'] = im_size
+    info['bbox_height'] = im_meta_dict['bbox'][3] - im_meta_dict['bbox'][1]
+    info['final_scale'] = final_scale
+    #info.torso_height = ?
+    #info.im_org_scaled = ?
+    info['padding'] = padding
+
+    #IMPORTANT NOTE: Since in tf and np, the first dimension is height and second width,
+    #swap x and y coordinates for target location
+    #For more info: 'Data formats' section in https://www.tensorflow.org/performance/performance_guide#use_nchw_imag
+    temp_coord = target_loc[0]
+    target_loc[0] = target_loc[1]
+    target_loc[1] = temp_coord
+
+    return [im, target_loc, info]
+
+def getAugmentedImage(im, im_meta_dict):
+  (im, im_meta_dict) = lrFlipCDHDDataRecord(im, im_meta_dict)
+  targets = [im_meta_dict['gt_x_coord'], im_meta_dict['gt_y_coord']]  
+
+  #add random scale jitter
+  scale_range = [round(log(0.6),4), round(log(1.25),4)]
+  scale = round(exp(random.uniform(0, 1) * (scale_range[1] - scale_range[0]) + scale_range[0]),2)
+
+  im = imresize(im, scale,interp='bilinear')
+  # im = skimage.transform.rescale(im, scale)
+  targets_np = np.zeros(2)
+  targets_np[0] = int(round(targets[0] * scale,0))
+  targets_np[1] = int(round(targets[1] * scale,0))
+  
+  #TO-DO: random crop jitter
+
+  return [im, targets_np, scale]
+
+def lrFlipCDHDDataRecord(im, im_meta_dict):
+  if random.uniform(0, 1) > 0.5:
+    im = np.fliplr(im)
+
+    #change x-coordinate of ground truth after flipping image
+    im_meta_dict['gt_x_coord'] = im.shape[1] - im_meta_dict['gt_x_coord']
+
+    #change x-coordinate of bounding box after flipping image
+    temp = im_meta_dict['bbox'][0]
+    im_meta_dict['bbox'][0] = im.shape[1] - im_meta_dict['bbox'][2]
+    im_meta_dict['bbox'][2] = im.shape[1] - temp
+
+  return im, im_meta_dict
+
+# Pads images such that all images in batch has greatest length and height among all images in the batch 
+def concatWithPadding(images, im_sizes, target_locs, bbox_heights):
+  max_dim = np.amax(im_sizes, axis=0)[0:2]
+
+  paddings = np.zeros([len(images), 4])
+  padded_images = []
+  warped_target_locs = []
+  warped_bbox_heights = []
+
+  #TODO: find a way to perform following two steps in one operations
+  paddings[:,0::3] = np.subtract(max_dim, im_sizes[:,:2])
+  paddings[:, 0], paddings[:, 1] = paddings[:, 1], paddings[:, 0].copy()
+
+  paddings = paddings.astype(int)
+
+  for idx in xrange(len(images)):
+    padding_tuple = (((paddings[idx])[0], (paddings[idx])[1]), ((paddings[idx])[2], (paddings[idx])[3]), (0,0))
+    padded_image = np.pad(images[idx], padding_tuple,'edge')
+    resized_img = skimage.transform.resize(padded_image, (224, 224))
+    padded_images.append(resized_img)
+
+    warp_ratio_1 = round(float(224)/(padded_image.shape[0]),2)
+    warp_ratio_2 = round(float(224)/(padded_image.shape[1]),2)
+
+    warped_target_locs.append([target_locs[idx][0] * warp_ratio_1, target_locs[idx][1] * warp_ratio_2])
+
+    # TODO: check if the below is correct or if warp_ratio_2 is he right multiplier
+    warped_bbox_heights.append(bbox_heights[0] * warp_ratio_1)
+
+    # Use to write image to disk with when skinage is used, works when mean is disabled
+    #skimage.io.imsave('car_ims_before/img_' + str(idx) + '.jpeg', padded_image)
+    #skimage.io.imsave('car_ims_after/img_' + str(idx) + '.jpeg', resized_img)
+
+    # Use to write image to disk with when PIL image is used
+    # Image.fromarray(np.pad(images[idx], padding_tuple,'edge')).save('car_ims/img_' + str(idx) + '.jpg') #DELETE
+
+  return np.asarray(padded_images), paddings, warped_target_locs, warped_bbox_heights
+
+def computePaddingForImage(im_size, stride):
+  padding = np.remainder(im_size[0:2],stride)
+  padding = np.remainder(np.subtract(stride, padding[0:2]), stride)
+  return padding
+
